a3d73c9f553dc17b8e223af48df92456a4a141de
amoschanGH <chanhinchung334@gmail.com>
2025-11-09 18:28:09 +0800

    chore: basic setup for the server framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_attribute]
+pub fn get(_attr: TokenStream, _item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(_item as DeriveInput);
+    quote::quote! { #input }.into()
 }
