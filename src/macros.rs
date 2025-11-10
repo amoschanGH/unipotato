90ccaa88d201ab2fb94b8762e97a821bf5d65726
amoschanGH <chanhinchung334@gmail.com>
2025-11-10 14:14:52 +0800

    feat: simple server is created

--- src/macros.rs
+++ src/macros.rs
@@ -1,25 +1,27 @@
 #[macro_export]
-macro_rules! get {
-    ($path:expr, $handler:expr) => {
+macro_rules! register_get {
+    ($path:expr, $handler:ident) => {
         {
             use std::sync::Arc;
-            let handler_arc = Arc::new(|req: hyper::Request<hyper::body::Incoming>| {
-                Box::pin($handler(req)) as std::pin::Pin<Box<dyn std::future::Future<Output = hyper::Response<String>> + Send>>
+            use hyper::{Request, Response, Method, body::Incoming};
+            let handler_arc = Arc::new(|req: Request<Incoming>| {
+                Box::pin($handler(req)) as std::pin::Pin<Box<dyn std::future::Future<Output = Response<String>> + Send>>
             });
-            $crate::Route::new(hyper::Method::GET, $path, handler_arc);
+            $crate::Route::new(Method::GET, $path, handler_arc);
         }
     };
 }
 
 #[macro_export]
-macro_rules! post {
-    ($path:expr, $handler:expr) => {
+macro_rules! register_post {
+    ($path:expr, $handler:ident) => {
         {
             use std::sync::Arc;
-            let handler_arc = Arc::new(|req: hyper::Request<hyper::body::Incoming>| {
-                Box::pin($handler(req)) as std::pin::Pin<Box<dyn std::future::Future<Output = hyper::Response<String>> + Send>>
+            use hyper::{Request, Response, Method, body::Incoming};
+            let handler_arc = Arc::new(|req: Request<Incoming>| {
+                Box::pin($handler(req)) as std::pin::Pin<Box<dyn std::future::Future<Output = Response<String>> + Send>>
             });
-            $crate::Route::new(hyper::Method::POST, $path, handler_arc);
+            $crate::Route::new(Method::POST, $path, handler_arc);
         }
     };
 }
